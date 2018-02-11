566fb70dd101178479a1f830ca5a2128b414badd
hadijahkyampeire <hadijah315@gmail.com>
2018-02-11 23:06:11 +0300

    Add edit functionality

--- src/components/categories/dashboard.js
+++ src/components/categories/dashboard.js
@@ -1,121 +1,180 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import {notify} from 'react-notify-toast';
 import axiosInstance from '../Apicalls';
 import CreateCategory from './CreateCategory';
+class EditCategory extends Component {
+    state = {
+        name:''
+    }
+    handleEditCategory =(event) =>{
+        event.preventDefault();
+        this.props.editCategory(this.props.id, this.state.name)
+    }
+    handleInput =(event) =>{
+        const {name, value} = event.target;
+        this.setState({[name]:value});
+    }
+    componentDidMount(){
+        this.setState({name:this.props.name})
+    }
+    render() {
+        return (
+            <div
+                className="modal fade"
+                id={`edit_category${this.props.id}`}
+                role="dialog"
+                aria-labelledby="exampleModalLabel"
+                aria-hidden="true">
+                <div className="modal-dialog" role="document">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title">Edit category name</h5>
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <form onSubmit={this.handleEditCategory}>
+                        <div className="modal-body">
+                            <div className="form-group">
+                                <input type="text" name="name" onChange={this.handleInput} className='form-control' value={this.state.name}/>
+                            </div>
 
-const Category =(props)=>(
+                        </div>
+                        <div className="modal-footer">
+                            <button type="submit" className="btn btn-primary" >Update</button>
+                            <button type="button" className="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                        </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+const Category = (props) => (
     <div className="col-md-3 col-sm-6 category-card">
-    <div className="card " >
-  <div className="card-block">
-    <h5 className="card-title">{props.name}</h5>
-    <Link className="btn btn-sm btn-success card-link" data-toggle="modal" data-target={`#edit_category${props.id}`} to="#"><i className="fa fa-edit"/></Link>
-    <Link className="btn btn-sm btn-danger card-link" onClick = {props.deleteCategory} to={`#`}><i className="fa fa-trash"/></Link>
-    <Link to={`/category/${props.id}/recipes`}><h5 className="card-footer">View Recipes</h5></Link>
-    <div className="modal fade" id={`edit_category${props.id}`} role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div className="modal-dialog" role="document">
-                <div className="modal-content">
-                <div className="modal-header">
-                    <h5 className="modal-title">Edit category name</h5>
-                    <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div className="modal-body">
-                <div className="form-group">
-                             <input
-                                type="text"
-                                name="name"
-                                className='form-control'
-                                value={props.name}/>
-                                    </div>
-
-                </div>
-                <div className="modal-footer">
-                    <button type="button" className="btn btn-primary" onClick = {props.editCategory}>Update</button>
-                    <button type="button" className="btn btn-secondary" data-dismiss="modal">Cancel</button>
-      </div>
+        <div className="card ">
+            <div className="card-block">
+                <h5 className="card-title">{props.name}</h5>
+                <Link
+                    className="btn btn-sm btn-success card-link"
+                    data-toggle="modal"
+                    data-target={`#edit_category${props.id}`}
+                    to="#"><i className="fa fa-edit"/></Link>
+                <Link
+                    className="btn btn-sm btn-danger card-link"
+                    onClick={props.deleteCategory}
+                    to={`#`}><i className="fa fa-trash"/></Link>
+                <Link to={`/category/${props.id}/recipes`}>
+                    <h5 className="card-footer">View Recipes</h5>
+                </Link>
+                <EditCategory name={props.name} id={props.id} editCategory={props.editCategory}/>
+            </div> 
+        </div>
     </div>
-  </div>
-</div>
-  </div>
-</div>
-</div>
 
 )
 
 class Categories extends Component {
     state = {
-        categories: [],
+        categories: []
     }
-    getCategories = ()=>{
-        const headers={Authorization:`Bearer ${localStorage.getItem('accessToken')}`}
-        axiosInstance.get('categories/',{headers})
-        .then(response=>{
-            this.setState({
-                categories:response.data.categories
+    getCategories = () => {
+        const headers = {
+            Authorization: `Bearer ${localStorage.getItem('accessToken')}`
+        }
+        axiosInstance
+            .get('categories/', {headers})
+            .then(response => {
+                this.setState({categories: response.data.categories});
+                console.log(this.state.categories)
+
+            })
+            .catch(error => {
+                if (error.response) {
+                    const {status} = error.response;
+                    if(status === 404){
+                        this.setState({
+                            categories: []
+                        });
+                    }
+                } else if (error.request) {
+                    alert("Request not made")
+                }
+                console.log(error)
             });
-            console.log(this.state.categories)
-        
-        }).catch(error=>{
-            if (error.response) {
-                alert(error.response.data.message)
-            } else if (error.request) {
-                alert("Request not made")
-            }
-            console.log(error)
-        });
     }
-    
-    componentWillMount(){
-       this.getCategories();
-    }
-    deleteCategory(value){
-        const headers={Authorization:`Bearer ${localStorage.getItem('accessToken')}`}
-        axiosInstance.delete(`categories/${value}`,{headers})
-        .then(response=>{
-            notify.show(response.data.message, 'success', 4000);
-            this.getCategories();
-        })
-    }
-    editCategory(value){
-        const headers={Authorization:`Bearer ${localStorage.getItem('accessToken')}`}
-        axiosInstance.put(`categories/${value}`,{headers})
-        .then(response=>{
-            notify.show(response.data.message, 'success', 4000);
-            this.getCategories();
-        })
-    }
-  render() {
-      const categoryitems = this.state.categories.map(category=>(
-          <Category name={category.cat.name} id={category.cat.id} 
-          deleteCategory={() => this.deleteCategory(category.cat.id)} 
-          editCategory={() => this.editCategory(category.cat.id)} 
-          key={category.cat.id} />
 
-      ))
-      console.log(categoryitems)
-    return (
-      <div >
-        <CreateCategory getCategories={this.getCategories}/>
-        <div className="viewcategories">
-        <div className="row categories">
-            {this.state.categories.length
-            ?categoryitems:null}
+    componentWillMount() {
+        this.getCategories();
+    }
+    deleteCategory(value) {
+        const headers = {
+            Authorization: `Bearer ${localStorage.getItem('accessToken')}`
+        }
+        axiosInstance
+            .delete(`categories/${value}`, {headers})
+            .then(response => {
+                notify.show(response.data.message, 'success', 4000);
+                this.getCategories();
+            })
+    }
+    editCategory=  (id, name)=>{
+        const headers = {
+            Authorization: `Bearer ${localStorage.getItem('accessToken')}`
+        }
+        axiosInstance
+            .put(`categories/${id}`,{name} ,{headers})
+            .then(response => {
+                notify.show(response.data.message, 'success', 4000);
+                this.getCategories();
+            })
+    }
+    render() {
+        const categoryitems = this
+            .state
+            .categories
+            .map(category => (<Category
+                name={category.cat.name}
+                id={category.cat.id}
+                deleteCategory={() => this.deleteCategory(category.cat.id)}
+                editCategory={this.editCategory}
+                key={category.cat.id}/>))
+        console.log(categoryitems)
+        return (
+            <div >
+                <CreateCategory getCategories={this.getCategories}/>
+                <div className="viewcategories">
+                    <div className="row categories">
+                        {this.state.categories.length
+                            ? categoryitems
+                            : <div className="col-sm-2 offset-sm-5">No categories </div>}
 
-        </div>
-        <div id="pagination">
-  <ul className="pagination">
-    <li className="active"><a href="#">1</a></li>
-    <li ><a href="#">2</a></li>
-    <li><a href="#">3</a></li>
-    <li><a href="#">4</a></li>
-    <li><a href="#">5</a></li>
-  </ul>
-  </div>
-      </div>
-      </div>
-    );
-  }
+                    </div>
+                    <div id="pagination">
+                        <ul className="pagination">
+                            <li className="active">
+                                <a href="#">1</a>
+                            </li>
+                            <li >
+                                <a href="#">2</a>
+                            </li>
+                            <li>
+                                <a href="#">3</a>
+                            </li>
+                            <li>
+                                <a href="#">4</a>
+                            </li>
+                            <li>
+                                <a href="#">5</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 export default Categories;
--- src/components/recipes/ViewRecipes.js
+++ src/components/recipes/ViewRecipes.js
@@ -4,31 +4,92 @@ import {notify} from 'react-notify-toast';
 import CreateRecipe from './CreateRecipes';
 import axiosInstance from '../Apicalls';
 
+class EditRecipe extends Component{
+  state ={
+    title:'',
+    description:''
+  }
+  handleInput =(event) =>{
+    const {name, value} = event.target;
+    this.setState({[name]:value})
+  }
+  handleEditRecipe =(event) =>{
+    event.preventDefault();
+    this.props.editRecipe(this.props.id,this.state.title,this.state.description)
+  }
+  componentDidMount(){
+    this.setState({title:this.props.title, description:this.props.description})
+}
+  render(){
+    return(
+      <div
+          className="modal fade"
+          id={`edit_recipe${this.props.id}`}
+          role="dialog"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true">
+          <div className="modal-dialog" role="document">
+              <div className="modal-content">
+                  <div className="modal-header">
+                      <h5 className="modal-title">Edit a recipe</h5>
+                      <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                          <span aria-hidden="true">&times;</span>
+                      </button>
+                  </div>
+                  <form onSubmit={this.handleEditRecipe}>
+                  <div className="modal-body">
+                      <div className="form-group">
+                          <input type="text" name="title" onChange={this.handleInput} className='form-control' value={this.state.title}/>
+                      </div>
+                      <div className="form-group">
+                              <textarea
+                                  rows="4"
+                                  cols="50"
+                                  name="description"
+                                  className="form-control description"
+                                  placeholder="description or instructions"
+                                  onChange={this.handleInput}
+                                  value={this.state.description}
+                                >
+                                </textarea>
+                            </div>
+                  </div>
+                  <div className="modal-footer">
+                      <button type="submit" className="btn btn-primary" >Update</button>
+                      <button type="button" className="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                  </div>
+                  </form>
+              </div>
+          </div>
+      </div>
+    );
+  }
+}
 const Recipe = (props) => (
   <div class="card">
     <div class="card-header" role="tab" id={`recipe-${props.id}`}>
-      <h5 class="mb-0">
-        <a
+      <h3 class="mb-0">
+        <button className="btn btn-link"
           data-toggle="collapse"
-          data-parent="#accordion"
           href={`#recipeDetails${props.id}`}
           aria-expanded="true"
           aria-controls="collapseOne">
           {props.title}
-        </a>
-      </h5>
+        </button>
+      </h3>
     </div>
 
     <div
       id={`recipeDetails${props.id}`}
-      class="collapse show"
+      class="collapse"
       role="tabpanel"
       aria-labelledby={`recipe-${props.id}`}>
       <div class="card-block">
         {props.description}
       </div>
-      <button className="btn btn-lg " data-toggle="modal" data-target="#edit_category" to="#"><i className="fa fa-edit"/></button>
+      <button className="btn btn-lg " data-toggle="modal" data-target={`#edit_recipe${props.id}`} to="#"><i className="fa fa-edit"/></button>
     <button className="btn btn-lg " onClick={props.deleteRecipe} to={`#`}><i className="fa fa-trash" to="#"/></button>
+    <EditRecipe title={props.title} description={props.description} id={props.id} editRecipe={props.editRecipe}/>
     </div>
   </div>
 )
@@ -53,7 +114,12 @@ class ViewRecipes extends Component {
       })
       .catch((error) => {
         if (error.response) {
-          alert(error.response.data.message)
+          const {status}= error.response;
+          if(status === 404){
+            this.setState({
+              recipes:[]
+            });
+          }
         } else if (error.request) {
           alert("Request not made")
         }
@@ -74,12 +140,25 @@ class ViewRecipes extends Component {
       this.getRecipes();
   })
   }
+  editRecipe =(id,title,description)=>{
+    const headers = {
+      Authorization: `Bearer ${localStorage.getItem('accessToken')}`
+  }
+      let category_id = this.props.match.params.id
+      axiosInstance.put(`categories/${category_id}/recipes/${id}`,{title,description},{headers})
+      .then(response =>{
+        this.getRecipes();
+      })
+
+  }
 
   render() {
     const recipeitems = this
       .state
       .recipes
-      .map(recipe => (<Recipe {...recipe.recipe} key={recipe.recipe.id} deleteRecipe={() => this.deleteRecipe(recipe.recipe.id)}/>));
+      .map(recipe => (<Recipe {...recipe.recipe} key={recipe.recipe.id} 
+        deleteRecipe={() => this.deleteRecipe(recipe.recipe.id)} 
+      editRecipe={this.editRecipe}/>));
 
     return (
       <div >
