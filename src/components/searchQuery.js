9161434803cf205bf5c4eb44a9e2e3d6936dc8f9
hadijahkyampeire <hadijah315@gmail.com>
2018-02-19 23:56:06 +0300

    [ft 155121618 search and pagination] implemented search query for category

---
+++ src/components/searchQuery.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class Search extends Component {
+    state = {
+        q: ''
+    }
+
+    static propTypes = {
+        handleSearch: PropTypes.func.isRequired,
+    }
+    
+    handleSearchInput = (event) => {
+        event.preventDefault();
+        this.props.handleSearch(this.state.q);
+    }
+    // implement search
+    handleInputChange = (event) => {
+        const {name, value} = event.target;
+        this.setState({[name]: value},()=>{
+        this.props.handleSearch(this.state.q);
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSearchInput} className ="form-group" role="search">
+                    <div className="input-group add-on col-sm-3 pull-right">
+                        <input
+                            type="search"
+                            value
+                            ={this.state.q}
+                            name="q"
+                            className="form-control"
+                            onChange={this.handleInputChange}
+                            placeholder="Search for a category..."/>
+                        <div className="input-group-btn">
+                            <button className="btn btn-default" type="submit">
+                                <i className="glyphicon glyphicon-search"></i>
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+export default Search;
