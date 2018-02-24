6bcf3276665eeb156f8f225d797273ba5c7f0730
hadijahkyampeire <hadijah315@gmail.com>
2018-02-24 23:34:47 +0300

    Modify site root

--- src/components/Apicalls.js
+++ src/components/Apicalls.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://127.0.0.1:5000/api/v1/',
+  baseURL: ' https://hadijahyummyrecipe-api.herokuapp.com/api/v1/',
   headers: { Authorization: `Bearer ${localStorage.getItem('accessToken')}` },
 });
 
