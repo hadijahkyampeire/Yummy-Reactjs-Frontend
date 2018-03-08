6cd20954c536bc9ef5e119eee47c87f6461ab548
kyampeire hadijah <khadijah@kyampeires-MacBook-Pro.local>
2018-03-08 20:16:56 +0300

    modifies file name

---
+++ src/__tests__/createRecipe.test.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import { MemoryRouter } from 'react-router-dom';
+import toJson, { shallowToJson } from 'enzyme-to-json';
+import sinon from 'sinon';
+
+import CreateRecipe from '../components/recipes/CreateRecipes';
+
+describe('CreateRecipe component', () => {
+  const wrapper = shallow(<CreateRecipe match={{params:{id:1}}} />);
+  const preventDefault = jest.fn();
+
+  it('renders properly', () => {
+    expect(shallowToJson(wrapper)).toMatchSnapshot();
+  });
+
+  it('has initial state', () => {
+    expect(wrapper.state().title).toEqual('');
+    expect(wrapper.state().description).toEqual('');
+  });
+
+  it('updates state', () => {
+    wrapper.setState({ title: 'lunch', description: 'cook' });
+    expect(wrapper.find('[name="title"]').props().value).toEqual('lunch');
+    expect(wrapper.find('[name="description"]').props().value).toEqual('cook');
+  });
+
+  it('has the correct form fields', () => {
+    expect(wrapper.find('[name="title"]')).toHaveLength(1);
+    expect(wrapper.find('[name="description"]')).toHaveLength(1);
+  });
+
+  it('has component in divs', () => {
+    expect(wrapper.find('div')).toHaveLength(9);
+  });
+
+  it('renders component in spans', () => {
+    expect(wrapper.find('span')).toHaveLength(2);
+  });
+
+  it('renders component in h2', () => {
+    expect(wrapper.find('h2')).toHaveLength(1);
+  });
+
+  it('has input field', () => {
+    expect(wrapper.find('input')).toHaveLength(2);
+  });
+
+  it('has breaks br field', () => {
+    expect(wrapper.find('br')).toHaveLength(3);
+  });
+
+  it('renders a form', () => {
+    expect(wrapper.find('form')).toHaveLength(1);
+    expect(wrapper.find('form').simulate('submit', {preventDefault}))
+    expect(preventDefault).toBeCalled();
+    expect(wrapper.find('[name="title"]').simulate('change', {target:{name:'title', value:'hi'}}))
+  });
+});
